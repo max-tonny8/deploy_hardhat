395693308d4b215c448d57a66bd0e259ea1e2e5c
tonny0831 <tonnyjansen0831@gmail.com>
2022-08-17 10:16:14 -0300

    deploy whitelist contract

---
+++ scripts/deploy.ts
@@ -0,0 +1,37 @@
+import { ethers, network } from "hardhat";
+import { writeFileSync, readFileSync } from "fs";
+import * as path from "path";
+
+async function main() {
+  const chainId = network.config.chainId;
+  console.log("chain", chainId);
+  const Whitelist = await ethers.getContractFactory("Whitelist");
+  const whitelist = await Whitelist.deploy(10);
+  await whitelist.deployed();
+
+  console.log("Whitelist deployed to:", whitelist.address);
+
+  const whitelistJson = JSON.parse(
+    readFileSync(
+      path.resolve(__dirname, "../", "lib", "Whitelist.json"),
+      "utf8"
+    )
+  );
+
+  const whitelistAbi = {
+    address: whitelist.address,
+    abi: JSON.parse(whitelist.interface.format("json") as string),
+  };
+
+  whitelistJson[chainId!.toString()] = whitelist;
+
+  writeFileSync(
+    path.resolve(__dirname, "../", "lib", "Whitelist.json"),
+    JSON.stringify(whitelistAbi, null, 2)
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
